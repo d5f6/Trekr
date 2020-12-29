f690bc8c9ac011a2f6a78430a70e283b68af8291
df <git@dfreitag.de>
2020-12-29 19:25:13 +0100

    finished loading data from json

--- Trekr/Location.swift
+++ Trekr/Location.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-struct Location {
+struct Location: Decodable {
     let id: Int
     let name: String
     let country: String
     let description: String
     let more: String
     let latitude: Double
-    let lonitude: Double
+    let longitude: Double
     let heroPicture: String
     let advisory: String
     
-    static let example = Location(id: 1, name: "Great Smokey Mountains", country: "United States", description: "A great Place to visit", more: "More text here.", latitude: 35.6532, lonitude: -83.5070, heroPicture: "smokies", advisory: "Beware of the bears!")
+    static let example = Location(id: 1, name: "Great Smokey Mountains", country: "United States", description: "A great Place to visit", more: "More text here.", latitude: 35.6532, longitude: -83.5070, heroPicture: "smokies", advisory: "Beware of the bears!")
 }
--- Trekr/Locations.swift
+++ Trekr/Locations.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class Locations: ObservableObject {
+    let places: [Location] // array
     
+    var primary: Location {
+        places[0]
+    }
+
+    init() {
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        
+        // convert the date into Location Arrays
+        places = try! JSONDecoder().decode([Location].self, from: data)
+    }
 }
